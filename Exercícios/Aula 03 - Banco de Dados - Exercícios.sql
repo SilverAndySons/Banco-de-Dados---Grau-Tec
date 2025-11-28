f00d94e09f399d18ae13f2da3e3a6dc08f1fa834
Anderson Peres <jpdandersonalencar@gmail.com>
2025-11-28 08:35:56 -0300

    Extra - 2025_11_28

--- Exercícios/Aula 03 - Banco de Dados - Exercícios.sql
+++ Exercícios/Aula 03 - Banco de Dados - Exercícios.sql
@@ -1,20 +1,39 @@
-CREATE TABLE alunos(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(50),
-    idade int,
-    serie varchar(50)
+create database escola;
+use escola;
+
+create table alunos(
+	id int primary key auto_increment,
+    nome varchar(50) not null,
+    idade int not null,
+    serie int not null
 );
 
-INSERT INTO alunos(nome, idade, serie) VALUES
-("Anderson",13,"9º ano"),
-("Allan",18,"3º ano médio"),
-("Laryssa",12,"8º ano"),
-("Fulano",5,"1º ano"),
-("Cicrano",12,"8º ano");
+insert into alunos(nome,idade,serie) values
+('Anderson',13,9),
+('Allan',18,3),
+('Laryssa',12,8),
+('Fulano',5,1),
+('Cicrano',12,8);
 
-SELECT * FROM alunos;
-SELECT nome, idade FROM alunos;
-SELECT * FROM alunos WHERE serie="8º ano";
-UPDATE alunos SET idade = 13 WHERE nome="Laryssa";
-DELETE FROM alunos WHERE nome="Allan";
-SELECT * FROM alunos;
+select * from alunos;
+select nome,idade from alunos;
+select * from alunos where serie = 8;
+update alunos set idade=7 where nome='Allan';
+delete from alunos where id=4;
+
+create table professores(
+	id int primary key auto_increment,
+    nome varchar(50) not null,
+    materia varchar(20) not null,
+    ano_ingresso int not null
+);
+
+alter table professores modify column materia varchar(50) not null;
+
+insert into professores(nome,materia,ano_ingresso) values
+('Gabriel','História da Computação',2019),
+('Ramon','Lógica de Programação',2022),
+('Walney','Informática Básica',2017),
+('Ana Paula','Banco de Dados',2025);
+
+select * from professores;
